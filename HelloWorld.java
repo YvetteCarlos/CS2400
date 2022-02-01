d50d5cf14b2d6eb1f4927a1c1b3efe0d25355b41
YvetteCarlos <yccarlos@cpp.edu>
2022-01-31 22:45:18 -0800

    I modified enter a number and your number

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.print("Please enter a number: ");
+        System.out.print("Please Enter A Number: ");
         int number = reader.nextInt();
-        System.out.println("The number you entered: " + number);
+        System.out.println("The Number You Entered: " + number);
     }
 }
 
